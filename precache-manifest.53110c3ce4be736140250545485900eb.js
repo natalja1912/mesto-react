ad43fab3e81c0846523deaaa6e6bb2f3c074c93d
Natalia Pavlova <natalja1912@yandex.ru>
2020-09-14 16:29:30 +0300

    Updates

---
+++ precache-manifest.53110c3ce4be736140250545485900eb.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84264e5db58f1d19dc15394a2a0729f4",
+    "url": "/natalja1912/mesto-react.git/index.html"
+  },
+  {
+    "revision": "15f5f07c1018e72465bd",
+    "url": "/natalja1912/mesto-react.git/static/css/main.38059431.chunk.css"
+  },
+  {
+    "revision": "fd689bf95fcdc6f87d88",
+    "url": "/natalja1912/mesto-react.git/static/js/2.8b66efd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/natalja1912/mesto-react.git/static/js/2.8b66efd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f5f07c1018e72465bd",
+    "url": "/natalja1912/mesto-react.git/static/js/main.4ea66dce.chunk.js"
+  },
+  {
+    "revision": "12e8087a5c0239fa15f4",
+    "url": "/natalja1912/mesto-react.git/static/js/runtime-main.db13559c.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/natalja1912/mesto-react.git/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/natalja1912/mesto-react.git/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/natalja1912/mesto-react.git/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/natalja1912/mesto-react.git/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/natalja1912/mesto-react.git/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "a5e1eecae07839e1412d26ecac573fe1",
+    "url": "/natalja1912/mesto-react.git/static/media/Inter-Regular.a5e1eeca.woff"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/natalja1912/mesto-react.git/static/media/add-button.bdfb8ebb.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/natalja1912/mesto-react.git/static/media/close-button.c2d0901f.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "/natalja1912/mesto-react.git/static/media/delete-button.d8078343.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/natalja1912/mesto-react.git/static/media/edit-button.58afbe73.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/natalja1912/mesto-react.git/static/media/header-logo.855a8c98.svg"
+  },
+  {
+    "revision": "4a1ce1f1a6e526a15c9a42459d94ded7",
+    "url": "/natalja1912/mesto-react.git/static/media/kusto.4a1ce1f1.jpg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/natalja1912/mesto-react.git/static/media/like-button-black.5bbb05f7.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/natalja1912/mesto-react.git/static/media/like-button.6c74d9b5.svg"
+  }
+]);
